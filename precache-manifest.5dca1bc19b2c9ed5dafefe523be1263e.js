063c7419360cef5200681c818abcb5f3826e476c
Klaudia <klaudia.krzywda@gmail.com>
2020-05-12 12:07:22 +0100

    Updates

---
+++ precache-manifest.5dca1bc19b2c9ed5dafefe523be1263e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb64b1d243ba4e7c120a0fee08a000d4",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "5ee1a5a1124f0a005eaf",
+    "url": "/Calculator/static/css/main.f06e81f2.chunk.css"
+  },
+  {
+    "revision": "f430106e064a004fea3b",
+    "url": "/Calculator/static/js/2.7203cffc.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Calculator/static/js/2.7203cffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee1a5a1124f0a005eaf",
+    "url": "/Calculator/static/js/main.1b1f07d1.chunk.js"
+  },
+  {
+    "revision": "d47b06047ebb73063df7",
+    "url": "/Calculator/static/js/runtime-main.31c35a96.js"
+  }
+]);
